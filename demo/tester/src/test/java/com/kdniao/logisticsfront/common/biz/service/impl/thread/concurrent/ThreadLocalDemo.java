7217717944503c3fca41e0a4e73055534771e2c5
zhangjin <zhangjin@kdniao.com>
2019-04-26 18:33:24 +0800

    发货宝-fix bug

--- demo/tester/src/test/java/com/kdniao/logisticsfront/common/biz/service/impl/thread/concurrent/ThreadLocalDemo.java
+++
@@ -1,7 +0,0 @@
-package com.kdniao.logisticsfront.common.biz.service.impl.thread.concurrent;
-
-public class ThreadLocalDemo {
-    public static void main(String[] args) {
-
-    }
-}
---
+++ demo/tester/src/test/java/com/kdniao/logisticsfront/common/biz/service/impl/thread/local/ThreadLocalDemo.java
@@ -0,0 +1,9 @@
+package com.kdniao.logisticsfront.common.biz.service.impl.thread.local;
+
+public class ThreadLocalDemo {
+    private static ThreadLocal threadLocal = new ThreadLocal();
+
+    public static void main(String[] args) {
+
+    }
+}
