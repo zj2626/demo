88642ac2489afb65eac0ad2ed028742489e8cc34
zhangjin <zhangjin@kdniao.com>
2019-05-06 18:34:47 +0800

    fix (demo): 关于Synchronized加锁的测试

---
+++ demo/tester/src/test/java/com/kdniao/logisticsfront/common/biz/service/impl/thread/factory/SynchronizedTest.java
@@ -0,0 +1,49 @@
+package com.kdniao.logisticsfront.common.biz.service.impl.thread.factory;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class SynchronizedTest implements Runnable {
+    private String name = "f";
+    private Integer num = 0;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        SynchronizedTest syncTest = new SynchronizedTest();
+
+        ExecutorService service = Executors.newFixedThreadPool(5);
+        for (int i = 0; i < 5; i++) {
+            syncTest.setName(new String("abc" + i));
+            service.submit(syncTest);
+            Thread.sleep(1);
+        }
+        Thread.sleep(10000);
+        service.shutdown();
+
+        System.out.println(syncTest.num);
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 10000000; i++) {
+//            synchronized (getName()) {
+//                num++;
+//            }
+
+            synchronized (this) {
+                num++;
+            }
+
+//            synchronized (SynchronizedTest.class) {
+//                num++;
+//            }
+        }
+    }
+}
