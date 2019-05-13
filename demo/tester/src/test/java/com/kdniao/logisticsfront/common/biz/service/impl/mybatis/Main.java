15ac6ff7310dfcfd3f973380d9413c9ec4712801
zhangjin <zhangjin@kdniao.com>
2019-05-13 14:42:11 +0800

    fix (demo): 添加mysql配置

--- demo/tester/src/test/java/com/kdniao/logisticsfront/common/biz/service/impl/mybatis/Main.java
+++ demo/tester/src/test/java/com/kdniao/logisticsfront/common/biz/service/impl/mybatis/Main.java
@@ -1,11 +1,28 @@
 package com.kdniao.logisticsfront.common.biz.service.impl.mybatis;
 
+import com.kdniao.logisticsfront.common.biz.service.impl.mybatis.dal.mapper.UcAreaDOMapper;
+import com.kdniao.logisticsfront.common.biz.service.impl.mybatis.dal.model.UcAreaDO;
+import com.kdniao.logisticsfront.common.biz.service.impl.mybatis.dal.model.UcAreaDOExample;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         ApplicationContext applicationContext = new ClassPathXmlApplicationContext("classpath:applicationContext.xml");
-        System.out.println(applicationContext);
+
+        UcAreaDOMapper ucAreaDOMapper = (UcAreaDOMapper) applicationContext.getBean("areaDOMapper");
+        select(ucAreaDOMapper);
+    }
+
+    public static void select(UcAreaDOMapper ucAreaDOMapper) {
+        UcAreaDOExample example = new UcAreaDOExample();
+        UcAreaDOExample.Criteria criteria = example.createCriteria();
+        criteria.andAreaCodeEqualTo("630102");
+
+        List<UcAreaDO> list = ucAreaDOMapper.selectByExample(example);
+        System.out.println(list.size());
+        System.out.println(list);
     }
 }
