62f597440f7cca29d6d60ca80eadcfbd0c37675c
zhangjin <zhangjin@kdniao.com>
2019-05-13 16:50:23 +0800

    fix (demo): 未完: 行锁,表锁,sphinx,mycat,Hystrix

--- demo/tester/src/test/java/com/kdniao/logisticsfront/common/biz/service/impl/mybatis/Main.java
+++ demo/tester/src/test/java/com/kdniao/logisticsfront/common/biz/service/impl/mybatis/Main.java
@@ -6,23 +6,79 @@ import com.kdniao.logisticsfront.common.biz.service.impl.mybatis.dal.model.UcAre
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
-public class Main {
-    public static void main(String[] args) {
-        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("classpath:applicationContext.xml");
+public class Main implements Runnable {
+    public static ApplicationContext applicationContext;
+    private static Integer sum = 0;
+    private boolean ifWrite = false;
 
-        UcAreaDOMapper ucAreaDOMapper = (UcAreaDOMapper) applicationContext.getBean("areaDOMapper");
-        select(ucAreaDOMapper);
+    public Main(boolean ifWrite) {
+        this.ifWrite = ifWrite;
     }
 
-    public static void select(UcAreaDOMapper ucAreaDOMapper) {
-        UcAreaDOExample example = new UcAreaDOExample();
-        UcAreaDOExample.Criteria criteria = example.createCriteria();
-        criteria.andAreaCodeEqualTo("630102");
+    static {
+        applicationContext = new ClassPathXmlApplicationContext("classpath:applicationContext.xml");
+    }
 
-        List<UcAreaDO> list = ucAreaDOMapper.selectByExample(example);
-        System.out.println(list.size());
-        System.out.println(list);
+    public static void main(String[] args) throws InterruptedException {
+        Main readMain = new Main(false);
+        Main writeMain = new Main(true);
+
+        ExecutorService service = Executors.newFixedThreadPool(50);
+        List<Future<?>> futureTasks = new ArrayList<>();
+
+        long start = System.currentTimeMillis();
+        for (int i = 0; i < 10; i++) {
+            if (i % 2 == 0) {
+                futureTasks.add(service.submit(readMain));
+            } else {
+                futureTasks.add(service.submit(writeMain));
+            }
+            Thread.sleep(1);
+        }
+        try {
+            for (Future<?> futureTask : futureTasks) {
+                futureTask.get();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        long end = System.currentTimeMillis();
+        System.out.println((end - start) + " --- " + sum);
+
+        service.shutdown();
+    }
+
+    @Override
+    public void run() {
+        UcAreaDOMapper ucAreaDOMapper = (UcAreaDOMapper) applicationContext.getBean("areaDOMapper");
+
+        System.out.println(Thread.currentThread().getName());
+
+        long start = System.currentTimeMillis();
+        if (!ifWrite) {
+            UcAreaDOExample example = new UcAreaDOExample();
+            UcAreaDOExample.Criteria criteria = example.createCriteria();
+
+            List<UcAreaDO> list = ucAreaDOMapper.selectByExample(example);
+            System.out.println("list.size " + list.size());
+        } else {
+            UcAreaDO record = new UcAreaDO();
+            record.setId("10007.0");
+            record.setAreaCode("231282022");
+            record.setAreaName(UUID.randomUUID().toString());
+
+            int updateInt = ucAreaDOMapper.updateByPrimaryKey(record);
+            System.out.println("updateInt " + updateInt);
+        }
+
+        long end = System.currentTimeMillis();
+        System.out.println(Thread.currentThread().getName() + " --- " + (end - start) + "\n");
     }
 }
