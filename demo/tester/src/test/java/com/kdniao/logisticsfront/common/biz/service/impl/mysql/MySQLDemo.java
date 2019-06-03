fcca9dcdf62d3e20aea54350fcc28dff1ccf0449
zhangjin <zhangjin@kdniao.com>
2019-06-03 17:07:49 +0800

    fix (demo): 操作数据库

--- demo/tester/src/test/java/com/kdniao/logisticsfront/common/biz/service/impl/mysql/MySQLDemo.java
+++ demo/tester/src/test/java/com/kdniao/logisticsfront/common/biz/service/impl/mysql/MySQLDemo.java
@@ -2,10 +2,8 @@ package com.kdniao.logisticsfront.common.biz.service.impl.mysql;
 
 import org.junit.Test;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
+import java.util.UUID;
 
 public class MySQLDemo {
     // JDBC 驱动名及数据库 URL
@@ -16,8 +14,16 @@ public class MySQLDemo {
     private static final String USER = "root";
     private static final String PASS = "sql@kdn!123";
 
-    private String sql = "INSERT INTO testa (NAME, AGE) VALUES ('eee', 30)";
-    private String sql2 = "INSERT INTO testb (NAME, AGE) VALUES ('abcde', 12)";
+    private String sql = "INSERT INTO testa (NAME, AGE) VALUES";
+    private String sql2 = "INSERT INTO testb (NAME, AGE) VALUES";
+    private String sql3 = "SELECT t1.ID, t1.AGE FROM testa t1 JOIN testb t2 ON t1.AGE = t2.AGE WHERE t2.AGE < 35;";
+    private String sql4 = "SELECT t1.ID, t1.AGE FROM testa t1 JOIN (SELECT t2.AGE FROM testb t2 WHERE t2.AGE < 35) t2 WHERE t1.AGE = t2.AGE;";
+    private String sql5 = "SELECT t1.ID, t1.AGE FROM testa t1 WHERE AGE IN (SELECT t2.AGE FROM testb t2 WHERE t2.AGE < 35);";
+
+    @Test
+    public void test() {
+        System.out.println(Double.valueOf(Math.floor(Math.random() * 10000)).intValue());
+    }
 
     @Test
     public void test1() {
@@ -28,9 +34,13 @@ public class MySQLDemo {
             stmt = conn.createStatement();
 
             int rs = 1;
-            for (int i = 0; i < 100000; i++) {
-                stmt.executeUpdate(sql);
+            for (int i = 0; i < 1000; i++) {
+                StringBuilder stringBuilder = new StringBuilder(sql);
+                for (int j = 0; j < 1000; j++) {
+                    stringBuilder.append(String.format(" ('%s', %d) ", UUID.randomUUID().toString().substring(0, 5), Double.valueOf(Math.floor(Math.random() * 1500)).intValue())).append(",");
+                }
                 System.out.println(i);
+                stmt.executeUpdate(stringBuilder.toString().substring(0, stringBuilder.toString().length() - 1));
             }
 
             System.out.println("rs: " + rs);
@@ -54,9 +64,91 @@ public class MySQLDemo {
             stmt = conn.createStatement();
 
             int rs = 1;
-            for (int i = 0; i < 100000; i++) {
-                stmt.executeUpdate(sql2);
+            for (int i = 0; i < 1000; i++) {
+                StringBuilder stringBuilder = new StringBuilder(sql2);
+                for (int j = 0; j < 1000; j++) {
+                    stringBuilder.append(String.format(" ('%s', %d) ", UUID.randomUUID().toString().substring(0, 5), Double.valueOf(Math.floor(Math.random() * 1500)).intValue())).append(",");
+                }
                 System.out.println(i);
+                stmt.executeUpdate(stringBuilder.toString().substring(0, stringBuilder.toString().length() - 1));
+            }
+
+            System.out.println("rs: " + rs);
+        } catch (Exception se) {
+            se.printStackTrace();
+        } finally {
+            try {
+                closeConnection(conn, stmt);
+            } catch (ClassNotFoundException | SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Test
+    public void test3() {
+        Connection conn = null;
+        Statement stmt = null;
+        try {
+            conn = getConnection();
+            stmt = conn.createStatement();
+
+            ResultSet resultSet = stmt.executeQuery(sql3);
+            int rs = 0;
+            while (resultSet.next()) {
+                rs++;
+            }
+
+            System.out.println("rs: " + rs);
+        } catch (Exception se) {
+            se.printStackTrace();
+        } finally {
+            try {
+                closeConnection(conn, stmt);
+            } catch (ClassNotFoundException | SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Test
+    public void test4() {
+        Connection conn = null;
+        Statement stmt = null;
+        try {
+            conn = getConnection();
+            stmt = conn.createStatement();
+
+            ResultSet resultSet = stmt.executeQuery(sql4);
+            int rs = 0;
+            while (resultSet.next()) {
+                rs++;
+            }
+
+            System.out.println("rs: " + rs);
+        } catch (Exception se) {
+            se.printStackTrace();
+        } finally {
+            try {
+                closeConnection(conn, stmt);
+            } catch (ClassNotFoundException | SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Test
+    public void test5() {
+        Connection conn = null;
+        Statement stmt = null;
+        try {
+            conn = getConnection();
+            stmt = conn.createStatement();
+
+            ResultSet resultSet = stmt.executeQuery(sql5);
+            int rs = 0;
+            while (resultSet.next()) {
+                rs++;
             }
 
             System.out.println("rs: " + rs);
