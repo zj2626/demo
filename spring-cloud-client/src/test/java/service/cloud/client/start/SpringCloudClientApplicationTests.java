4df1f6338d694113bc5aef5fae46cf9785ccad05
zj2626 <zhangj2626>
2019-07-04 00:23:10 +0800

    fix (demo): Spring Cloud

---
+++ spring-cloud-client/src/test/java/service/cloud/client/start/SpringCloudClientApplicationTests.java
@@ -0,0 +1,16 @@
+package service.cloud.client.start;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringCloudClientApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
