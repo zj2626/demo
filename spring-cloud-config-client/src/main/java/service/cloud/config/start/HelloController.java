fb9b0d39298405bdb0d64a6762dcab4f6f9b2853
zhangjin <zhangjin@kdniao.com>
2019-07-08 11:43:47 +0800

    SpringCloud: Spring Cloud Bus 配置热更新

--- spring-cloud-config-client/src/main/java/service/cloud/config/start/HelloController.java
+++ spring-cloud-config-client/src/main/java/service/cloud/config/start/HelloController.java
@@ -1,9 +1,12 @@
 package service.cloud.config.start;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+// 不重启更新配置信息需要用到消息中间件
+@RefreshScope // spring cloud bus配置自动刷新需要在包含更新的@Value的类加上 @RefreshScope
 @RestController
 public class HelloController {
     @Value("${foo}")
