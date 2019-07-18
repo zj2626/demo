bd2b183ffc2bfe9d03de24b241ba161d6004b33c
zhangjin <zhangjin@kdniao.com>
2019-07-18 13:47:30 +0800

    算法: 快速排序

--- demo/src/main/java/com/kdniao/logisticsfront/common/biz/service/impl/algorithm/math/data/Arrayss.java
+++ demo/src/main/java/com/kdniao/logisticsfront/common/biz/service/impl/algorithm/math/data/Arrayss.java
@@ -14,17 +14,18 @@ public class Arrayss {
     public void init() {
         // 正向
         arr = new Integer[]{2, 1, 10, 5, 3, 4, 7, 9, 11, 8};
+//        arr = new Integer[]{52, 39, 67, 95, 70, 8, 25, 52, 8, 7, 99, 99, 21, 39, 52};
 
         // 反向
         ArrayUtils.reverse(arr);
 
         System.out.println("长度: " + arr.length);
-        System.out.println("操作前: " + Arrays.toString(arr) + "\n");
+        System.out.println("<操作前> \n" + Arrays.toString(arr) + "\n<操作前> \n");
     }
 
     @After
     public void print() {
-        System.out.println("\n" + "操作后: " + Arrays.toString(arr));
+        System.out.println("\n" + "<操作后> \n" + Arrays.toString(arr) + "\n<操作后>");
         System.out.println("次数: " + n);
     }
 }
--- demo/src/main/java/com/kdniao/logisticsfront/common/biz/service/impl/algorithm/math/sort/Quicksort.java
+++ demo/src/main/java/com/kdniao/logisticsfront/common/biz/service/impl/algorithm/math/sort/Quicksort.java
@@ -13,66 +13,133 @@ import java.util.Arrays;
  * 适合数据不大的情况
  */
 public class Quicksort extends Arrayss {
+    /*自己的*/
     @Test
     public void test() {
         quick(arr, 0, arr.length - 1);
-//        quickSort(arr, 0, arr.length - 1);
     }
 
-    public void quick(Integer[] arr, int start, int end) {
-        int key = arr[start];
-        System.out.println(key);
-        int min = start;
-        int max = end;
+    /*自己的*/
+    @Test
+    public void test2() {
+        quick2(arr, 0, arr.length - 1);
+    }
+
+    /*网上的1*/
+    @Test
+    public void test3() {
+        quickSort(arr, 0, arr.length - 1);
+    }
+
+    /*网上的2*/
+    @Test
+    public void test4() {
+        sort(arr, 0, arr.length - 1);
+    }
+
+    public void quick(Integer[] arr, int low, int high) {
+        // 1.先判断要排序的数组分区是数组的有效部分
+        if (low >= high) {
+            return;
+        }
+
+        // 2.得到基准值 默认取分区的第一个值
+        int key = arr[low];
+        int min = low;
+        int max = high;
 
         while (min < max) {
-            while (min < max && key < arr[max]) {
+            // 从尾部遍历 得到比基准值小的值的位置
+            while (key < arr[max]) {
                 max--;
             }
-            while (min < max && key > arr[min]) {
+
+            // 从头部遍历 得到比基准值大的值的位置
+            while (key > arr[min]) {
                 min++;
             }
 
-            if (min < max) {
-                int temp = arr[min];
-                arr[min] = arr[max];
-                arr[max] = temp;
-            }
-
+            exchange(arr, min, max);
         }
 
+        System.out.println(low + " - " + high + " | " + min + " - " + max);
         System.out.println(Arrays.toString(arr));
-        System.out.println(min + " - " + max);
+        System.out.println();
+    }
 
-        quick(arr, start, min - 1);
-        quick(arr, min + 1, end);
+    public void quick2(Integer[] arr, int low, int high) {
+        /**
+         * before [8, 11, 9, 7, 4, 3, 5, 10, 1, 2]
+         *
+         * base on: 8
+         * [2, 11, 9, 7, 4, 3, 5, 10, 1, 8]  [2, 8, 9, 7, 4, 3, 5, 10, 1, 11]
+         * [2, 1, 9, 7, 4, 3, 5, 10, 8, 11]  [2, 1, 8, 7, 4, 3, 5, 10, 9, 11]
+         * [2, 1, 5, 7, 4, 3, 8, 10, 9, 11]
+         *
+         * left: base on: 2
+         * [1, 2, 5, 7, 4, 3, 8, 10, 9, 11]
+         *
+         * left-right: base on: 5
+         * [1, 2, 3, 7, 4, 5, 8, 10, 9, 11]  [1, 2, 3, 5, 4, 7, 8, 10, 9, 11]
+         * [1, 2, 3, 4, 5, 7, 8, 10, 9, 11]
+         * left-right-left: base on: 3
+         * [1, 2, 3, 4, 5, 7, 8, 10, 9, 11]
+         *
+         * right: base on: 9
+         * [1, 2, 3, 4, 5, 7, 8, 9, 10, 11]
+         */
 
-//        if (min >= max) {
-//            int temp = min;
-//            arr[temp] = key;
-//
-//            if (temp > 0 && temp < arr.length - 1) {
-//
-//                key = arr[temp - 1];
-//                quick(arr, start, temp - 1, key);
-//
-//                key = arr[temp + 1];
-//                quick(arr, temp + 1, end, key);
-//            }
-//
-//        } else {
-//            quick(arr, min, max, key);
-//        }
+        // 1.先判断要排序的数组分区是数组的有效部分
+        if (low >= high) {
+            return;
+        }
 
-        System.out.println(min + "-" + max);
-//        if (min >= max) {
-//            int temp = min;
-//            arr[temp] = key;
-//        } else {
-//            quick(arr, min, max);
-//        }
+        // 2.得到基准值 默认取分区的第一个值
+        int key = arr[low];
+        int min = low;
+        int max = high;
 
+        while (min < max) {
+            // 从尾部遍历 得到比基准值小的值的位置
+            while (key <= arr[max] && min < max) {
+                max--;
+            }
+            //得到比基准值小的值 将其与基准值交换
+            if (min < max) {
+                // 位置: max -> min
+                exchange(arr, min, max);
+                System.out.print("> " + Arrays.toString(arr) + "\t");
+            }
+
+            // 从头部遍历 得到比基准值大的值的位置
+            while (key >= arr[min] && min < max) {
+                min++;
+            }
+            //得到比基准值大的值 将其与基准值交换
+            if (min < max) {
+                // 位置: min -> max
+                exchange(arr, min, max);
+                System.out.print(Arrays.toString(arr) + "\t");
+            }
+            System.out.println();
+            n++;
+        }
+
+        System.out.println(low + " - " + high + " | " + min + " - " + max);
         System.out.println(Arrays.toString(arr));
+        System.out.println();
+
+        //left
+        quick2(arr, low, min - 1);
+
+        //right
+        quick2(arr, min + 1, high);
+    }
+
+    private void exchange(Integer[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
 
     public static void quickSort(Integer[] arr, int low, int high) {
@@ -84,7 +151,6 @@ public class Quicksort extends Arrayss {
         j = high;
         //temp就是基准位
         temp = arr[low];
-        System.out.println(temp);
 
         while (i < j) {
             //先看右边，依次往左递减
@@ -101,18 +167,55 @@ public class Quicksort extends Arrayss {
                 arr[j] = arr[i];
                 arr[i] = t;
             }
+            n++;
 
         }
         //最后将基准为与i和j相等位置的数字交换
         arr[low] = arr[i];
         arr[i] = temp;
 
+        System.out.println(low + " - " + high + " | " + i + " - " + j);
         System.out.println(Arrays.toString(arr));
+        System.out.println();
 
         //递归调用左半数组
         quickSort(arr, low, j - 1);
         //递归调用右半数组
         quickSort(arr, j + 1, high);
+    }
+
+    private void sort(Integer[] a, int low, int high) {
+        if (low >= high) {
+            return;
+        }
+        int i = low, j = high; // 设置这两个变量的目的是为了保持low和high不变
+        int pivotNum = a[i]; // 基准数
+        while (i < j) {
+            while (a[j] >= pivotNum && j > i) { // 循环结束的条件有二：一是找到比支点小的数，二是j==i
+                j--;
+            }
+            if (j > i) { // 由于上面循环结束的功能性有两个，对于找到比支点小的数，即j!=i，要进行位置的交换，下同
+                a[i] = a[j];
+                i++;
+            }
+
+            while (a[i] < pivotNum && i < j) {
+                i++;
+            }
+            if (i < j) {
+                a[j] = a[i];
+                j--;
+            }
+            n++;
+        }
+        a[i] = pivotNum;
+
+        System.out.println(low + " - " + high + " | " + i + " - " + j);
+        System.out.println(Arrays.toString(arr));
+        System.out.println();
+
+        sort(a, low, i - 1);
+        sort(a, i + 1, high);
     }
 
 }
