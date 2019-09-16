4e01e45fe477219bceea03b3d318f50f4ea268a4
jin.zhang <jin.zhang@yitaichang.cn>
2019-09-16 14:58:03 +0800

    java.time包下时间工具

--- demo/src/main/java/com/kdniao/logisticsfront/common/biz/service/impl/date/TestJavaTime.java
+++ demo/src/main/java/com/kdniao/logisticsfront/common/biz/service/impl/date/TestJavaTime.java
@@ -1,6 +1,5 @@
 package com.kdniao.logisticsfront.common.biz.service.impl.date;
 
-import org.joda.time.DateTime;
 import org.junit.Test;
 
 import java.time.*;
@@ -8,7 +7,7 @@ import java.time.format.DateTimeFormatter;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
-public class TestTime {
+public class TestJavaTime {
     private static final String dtf = "yyyy-MM-dd";
     private static final String ttf = "HH:mm:ss";
     private static final String stf = "yyyy-MM-dd HH:mm:ss";
@@ -16,7 +15,7 @@ public class TestTime {
     private static final String timeStr = "11:11:11";
     private static final String dateTimeStr = "2019-01-21 11:11:11";
     private static final String datetTimeStr = "2019-01-21T11:11:11";
-
+    
     @Test
     public void testTime() {
         System.out.println(TimeUnit.DAYS.toHours(3));
@@ -31,18 +30,17 @@ public class TestTime {
         System.out.println(TimeUnit.MICROSECONDS.toNanos(1));
         System.out.println(TimeUnit.NANOSECONDS.toNanos(1));
     }
-
+    
+    // #################################################
+    //          下面都是线程安全的 best !!!!!!
+    // #################################################
+    
+    /**
+     * 当前时间
+     */
     @Test
-    public void testTime2() {
-        DateTime today = new DateTime();
-        String dtf_date = today.toString(stf);
-        System.out.println(dtf_date);
-    }
-
-    // 下面都是线程安全的 best !!!!!!
-    @Test
-    public void testTime3() {
-        // LocalDate
+    public void testNow() {
+        // LocalDate 当天
         LocalDate nowDate = LocalDate.now();
         System.out.println(nowDate);
         System.out.println(nowDate.getYear());
@@ -51,84 +49,106 @@ public class TestTime {
         System.out.println(nowDate.getDayOfMonth());
         System.out.println(nowDate.getDayOfWeek().getValue());
         System.out.println();
-
-        LocalDate znow = LocalDate.now(ZoneId.systemDefault());
-        System.out.println(znow);
-        System.out.println();
-
-        // LocalDate
-        LocalDate date = LocalDate.of(2019, 5, 12);
-        System.out.println(date);
-        LocalDate date2 = LocalDate.ofYearDay(2019, 210);
-        System.out.println(date2);
-        // ##############
-        LocalDate date3 = LocalDate.parse(dateStr);
-        System.out.println(date3);
-        System.out.println();
-        System.out.println();
-
-        // LocalTime
-        LocalTime noon = LocalTime.NOON;
-        System.out.println(noon);
+        
+        // LocalTime 当小时
         LocalTime nowTime = LocalTime.now();
         System.out.println(nowTime);
         System.out.println(nowTime.getHour());
         System.out.println(nowTime.getMinute());
         System.out.println(nowTime.getSecond());
         System.out.println();
-
-        // LocalTime
-        LocalTime time = LocalTime.ofSecondOfDay(1234);
-        System.out.println(time);
-        // ##############
-        LocalTime time2 = LocalTime.parse(timeStr);
-        System.out.println(time2);
-        System.out.println();
-        System.out.println();
-
-        // LocalDateTime
+        
+        // LocalDateTime 当天完整时间
         LocalDateTime nowDateTime = LocalDateTime.now();
         System.out.println(nowDateTime);
-        LocalDateTime dateTime = LocalDateTime.of(nowDate, nowTime);
-        System.out.println(dateTime);
-        LocalDateTime dateTime2 = LocalDateTime.of(2019, 6, 30, 12, 12, 12);
-        System.out.println(dateTime2);
+        
+        System.out.println("\n##################");
+        LocalDate znow = LocalDate.now(ZoneId.systemDefault());
+        System.out.println(znow);
         System.out.println();
-        // ##############
+    }
+    
+    /**
+     * 指定时间
+     */
+    @Test
+    public void testPointTime() {
+        // 2019年的第210天
+        LocalDate date2 = LocalDate.ofYearDay(2019, 210);
+        System.out.println(date2);
+        
+        // 中午
+        LocalTime noon = LocalTime.NOON;
+        System.out.println("NOON: " + noon);
+        
+        // 某天的第1234秒
+        LocalTime time = LocalTime.ofSecondOfDay(1234);
+        System.out.println(time);
+        
+        // 2019年5月12日
+        LocalDate date = LocalDate.of(2019, 5, 12);
+        System.out.println(date);
+        
+        // 2019年6月......
+        LocalDateTime dateTime2 = LocalDateTime.of(2019, 6, 30, 14, 56, 12);
+        System.out.println(dateTime2);
+        
+        // LocalDate 和 LocalTime 拼接成的 LocalDateTime
+        LocalDateTime dateTime = LocalDateTime.of(date2, time);
+        System.out.println(dateTime);
+    }
+    
+    /**
+     * 时间各种转化
+     */
+    @Test
+    public void testTransform() {
+        System.out.println("\n字符串 转化为 LocalDate");
+        LocalDate date = LocalDate.parse(dateStr);
+        System.out.println(date);
+        
+        System.out.println("\n字符串 转化为 LocalTime");
+        LocalTime time2 = LocalTime.parse(timeStr);
+        System.out.println(time2);
+        
+        System.out.println("\n字符串 转化为 LocalDateTime");
         LocalDateTime dateTime3 = LocalDateTime.parse(datetTimeStr);
         System.out.println(dateTime3);
+        
+        System.out.println("\nLocalDateTime 转化为 字符串");
         DateTimeFormatter dtf1 = DateTimeFormatter.ofPattern(stf);
         String dateTimeStr1 = dtf1.format(dateTime3);
         System.out.println(dateTimeStr1);
+        
+        System.out.println("\nLocalDateTime 转化为 字符串");
         DateTimeFormatter dtf2 = DateTimeFormatter.ofPattern(ttf);
         String dateTimeStr2 = dtf2.format(dateTime3);
         System.out.println(dateTimeStr2);
+        
+        System.out.println("\nLocalDateTime 转化为 字符串");
         DateTimeFormatter dtf3 = DateTimeFormatter.ofPattern(dtf);
         String dateTimeStr3 = dtf3.format(dateTime3);
         System.out.println(dateTimeStr3);
-        DateTimeFormatter dtf4 = DateTimeFormatter.ofPattern(stf);
-        String dateTimeStr4 = dtf4.format(LocalDateTime.parse(datetTimeStr));
-        System.out.println(dateTimeStr4);
-        System.out.println();
-
-        // Date转换为LocalDateTime
+        
+        System.out.println("\n#####################");
         Date utilDate = new Date();
-        Instant instant = utilDate.toInstant();
+        
+        System.out.println("\njava.util.Date 转化为 LocalDateTime");
         ZoneId zoneId = ZoneId.systemDefault(); // 默认时区
+        Instant instant = utilDate.toInstant();
+        System.out.println("UtilDate: " + utilDate);
         // 方法一
         LocalDateTime localDateTime = instant.atZone(zoneId).toLocalDateTime();
+        System.out.println("LocalDateTime: " + localDateTime);
         // 方法二
         LocalDateTime localDateTime2 = LocalDateTime.ofInstant(utilDate.toInstant(), zoneId);
-        System.out.println(utilDate);
-        System.out.println(localDateTime);
-        System.out.println(localDateTime2);
-        System.out.println();
-
-        // LocalDateTime转换为Date
+        System.out.println("LocalDateTime: " + localDateTime2);
+        
+        System.out.println("\nLocalDateTime 转化为 java.util.Date");
         ZonedDateTime zonedDateTime = localDateTime.atZone(zoneId);
         Date utilDate2 = Date.from(zonedDateTime.toInstant());
-        System.out.println(localDateTime);
-        System.out.println(zonedDateTime);
-        System.out.println(utilDate2);
+        System.out.println("LocalDateTime: " + localDateTime);
+        System.out.println("ZonedDateTime: " + zonedDateTime);
+        System.out.println("UtilDate: " + utilDate2);
     }
 }
