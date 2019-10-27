12efc96e96687caf60eca6aef23a534b673e3d1f
zj2626 <zhangj2626>
2019-10-27 23:04:04 +0800

    删除多余的

--- spring-cloud-client-parent/spring-cloud-client/src/main/java/service/cloud/client/start/service/InterfaceFeignService.java
+++ spring-cloud-client-parent/spring-cloud-client/src/main/java/service/cloud/client/start/service/InterfaceFeignService.java
@@ -1,10 +1,9 @@
 package service.cloud.client.start.service;
 
 import org.springframework.cloud.openfeign.FeignClient;
-import service.cloud.client.start.service.fallback.SchedualServiceHiHystric;
 import service.cloud.client2.start.controller.api.ControllerApi;
 
-//@FeignClient(value = "spring-cloud-client02")
-@FeignClient(value = "spring-cloud-client02", fallback = SchedualServiceHiHystric.class)
+@FeignClient(value = "spring-cloud-client02")
+//@FeignClient(value = "spring-cloud-client02", fallback = SchedualServiceHiHystric.class)
 public interface InterfaceFeignService extends ControllerApi {
 }
--- spring-cloud-client-parent/spring-cloud-client/src/main/java/service/cloud/client/start/service/fallback/SchedualServiceHiHystric.java
+++ spring-cloud-client-parent/spring-cloud-client/src/main/java/service/cloud/client/start/service/fallback/SchedualServiceHiHystric.java
@@ -1,43 +1,43 @@
-package service.cloud.client.start.service.fallback;
-
-import org.springframework.stereotype.Component;
-import service.cloud.client.start.service.InterfaceFeignService;
-
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-
-/**
- * 熔断以后执行这里的方法
- */
-@Component
-public class SchedualServiceHiHystric implements InterfaceFeignService {
-    @Override
-    public String fun(String name) {
-        return "fallback to here method[fun]";
-    }
-
-    @Override
-    public String fun2(Boolean success) {
-        return "fallback to here method[fun2]";
-    }
-
-    @Override
-    public String hi(String name) {
-        return "fallback to here method[doServiceRequestHi]";
-    }
-
-    @Override
-    public String doHalfFailed(Boolean success) {
-        return "fallback to here method[doServiceRequestHalfFailed]";
-    }
-
-    @Override
-    public String zipkinMethod2(String name) {
-        return "ERROR";
-    }
-
-    @Override
-    public String zipkinMethod4(String name) {
-        return "ERROR";
-    }
-}
+//package service.cloud.client.start.service.fallback;
+//
+//import org.springframework.stereotype.Component;
+//import service.cloud.client.start.service.InterfaceFeignService;
+//
+//import javax.servlet.http.HttpServletResponse;
+//import java.io.IOException;
+//
+///**
+// * 熔断以后执行这里的方法
+// */
+//@Component
+//public class SchedualServiceHiHystric implements InterfaceFeignService {
+//    @Override
+//    public String fun(String name) {
+//        return "fallback to here method[fun]";
+//    }
+//
+//    @Override
+//    public String fun2(Boolean success) {
+//        return "fallback to here method[fun2]";
+//    }
+//
+//    @Override
+//    public String hi(String name) {
+//        return "fallback to here method[doServiceRequestHi]";
+//    }
+//
+//    @Override
+//    public String doHalfFailed(Boolean success) {
+//        return "fallback to here method[doServiceRequestHalfFailed]";
+//    }
+//
+//    @Override
+//    public String zipkinMethod2(String name) {
+//        return "ERROR";
+//    }
+//
+//    @Override
+//    public String zipkinMethod4(String name) {
+//        return "ERROR";
+//    }
+//}
