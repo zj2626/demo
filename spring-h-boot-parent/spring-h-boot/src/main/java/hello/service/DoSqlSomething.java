521c5e1aec2d9c3fec052d1749facb2c6d647342
jin.zhang <jin.zhang@yitaichang.cn>
2019-10-30 15:28:02 +0800

    @ConditionalOnProperty控制Bean是否生效

--- spring-h-boot-parent/spring-h-boot/src/main/java/hello/service/DoSqlSomething.java
+++ spring-h-boot-parent/spring-h-boot/src/main/java/hello/service/DoSqlSomething.java
@@ -4,9 +4,11 @@ import hello.data.service.TestcDao;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Service;
 
 @Service
+@ConditionalOnProperty(value = "api.filter.enable", matchIfMissing = true)
 public class DoSqlSomething {
     @Autowired
     private TestcDao testcDao;
