6ceb24bd83abf6158166b668010cad566e02b2fa
jin.zhang <jin.zhang@yitaichang.cn>
2019-11-27 17:29:31 +0800

    logInfoMaker

--- spring-m-boot-parent/spring-m-boot/src/main/java/hello/aspect/LogAspect.java
+++ spring-m-boot-parent/spring-m-boot/src/main/java/hello/aspect/LogAspect.java
@@ -63,51 +63,40 @@ public class LogAspect {
      * @throws Throwable
      */
     @Order(2)
-    @Around("@annotation(hello.annotation.LogInfoMaker)")
-    public Object addLog(ProceedingJoinPoint joinPoint) throws Throwable {
+    @Around("@annotation(logInfoMaker)")
+    public Object addLog(ProceedingJoinPoint joinPoint, LogInfoMaker logInfoMaker) throws Throwable {
         Object[] args = joinPoint.getArgs();
-        LogInfoMaker LogInfoMaker = getLogInfoMaker(joinPoint);
-        if (isNotNull(LogInfoMaker)) {
-            logger.info(LogInfoMaker.value().toString(), makeLogForm(LogInfoMaker, "入参") + ":{}", makeLogMsg(args));
-        }
+        logger.info(logInfoMaker.value().toString(), makeLogForm(logInfoMaker, "入参") + ":{}", makeLogMsg(args));
         Object result;
         try {
             result = joinPoint.proceed(args);
         } catch (Exception ex) {
-            if (isNotNull(LogInfoMaker)) {
-                logger.error(LogInfoMaker.value().toString(), makeLogForm(LogInfoMaker, "异常"), ex);
-            }
+            logger.error(logInfoMaker.value().toString(), makeLogForm(logInfoMaker, "异常"), ex);
             throw ex;
         }
-        if (isNotNull(LogInfoMaker)) {
-            logger.info(LogInfoMaker.value().toString(), makeLogForm(LogInfoMaker, "出参") + ":{}", makeLogMsg(result));
-        }
+        logger.info(logInfoMaker.value().toString(), makeLogForm(logInfoMaker, "出参") + ":{}", makeLogMsg(result));
         return result;
-    }
-
-    private LogInfoMaker getLogInfoMaker(JoinPoint joinPoint) {
-        LogInfoMaker LogInfoMaker = null;
-        try {
-            Method method = ((MethodSignature) joinPoint.getSignature()).getMethod();
-            boolean flag = method.isAnnotationPresent(LogInfoMaker.class);
-            if (flag) {
-                LogInfoMaker = method.getAnnotation(LogInfoMaker.class);
-            }
-        } catch (Exception e) {
-            logger.error("获取注解@LogInfoMaker异常 : " + e.getMessage());
-        }
-        return LogInfoMaker;
-    }
-
-    private boolean isNotNull(LogInfoMaker LogInfoMaker) {
-        return null != LogInfoMaker;
     }
 
     private String makeLogMsg(Object msg) {
         return "【" + JSON.toJSONString(msg) + "】";
     }
 
-    private String makeLogForm(LogInfoMaker LogInfoMaker, String entranceFlag) {
-        return LogInfoMaker.value().getValue() + "-" + LogInfoMaker.value().getDesc() + entranceFlag;
+    private String makeLogForm(LogInfoMaker logInfoMaker, String entranceFlag) {
+        return logInfoMaker.value().getValue() + "-" + logInfoMaker.value().getDesc() + entranceFlag;
+    }
+    
+    private LogInfoMaker getLogInfoMaker(JoinPoint joinPoint) {
+        LogInfoMaker logInfoMaker = null;
+        try {
+            Method method = ((MethodSignature) joinPoint.getSignature()).getMethod();
+            boolean flag = method.isAnnotationPresent(LogInfoMaker.class);
+            if (flag) {
+                logInfoMaker = method.getAnnotation(LogInfoMaker.class);
+            }
+        } catch (Exception e) {
+            logger.error("获取注解@LogInfoMaker异常 : " + e.getMessage());
+        }
+        return logInfoMaker;
     }
 }
