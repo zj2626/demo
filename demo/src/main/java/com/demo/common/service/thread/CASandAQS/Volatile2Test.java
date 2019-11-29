3d4e0c33f3185a606df34e3c4cdcdfd1d1ad9586
jin.zhang <jin.zhang@yitaichang.cn>
2019-11-29 09:25:29 +0800

    volatile

---
+++ demo/src/main/java/com/demo/common/service/thread/CASandAQS/Volatile2Test.java
@@ -0,0 +1,41 @@
+package com.demo.common.service.thread.CASandAQS;
+
+import com.demo.common.service.thread.CASandAQS.abs.Excutor;
+import com.demo.common.service.thread.CASandAQS.abs.ThreadDemo;
+import org.junit.Test;
+
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+public class Volatile2Test extends Excutor {
+    private volatile boolean ifRun = true;
+    private long number = 0;
+    
+    @Test
+    public void test() throws InterruptedException {
+        threadExcutor = new ThreadDemo(this);
+        threadExcutor.execute(2);
+        
+        stopThread();
+        
+        threadExcutor.futureGet();
+        System.out.println("结果 " + ifRun + " - " + number);
+    }
+    
+    @Override
+    public String doExcute(Map<String, String> parameter) throws Exception {
+        while (ifRun) {
+            number++;
+        }
+        return null;
+    }
+    
+    private void stopThread(){
+        try {
+            TimeUnit.MILLISECONDS.sleep(10);
+        } catch (InterruptedException ignored) {
+        }
+        
+        ifRun = false;
+    }
+}
