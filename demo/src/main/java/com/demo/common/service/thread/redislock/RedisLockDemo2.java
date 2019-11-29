1c51d774d00052ed4525472b4c43316971222000
jin.zhang <jin.zhang@yitaichang.cn>
2019-11-29 10:39:36 +0800

    Redisson

--- demo/src/main/java/com/demo/common/service/thread/redislock/RedisLockDemo2.java
+++ demo/src/main/java/com/demo/common/service/thread/redislock/RedisLockDemo2.java
@@ -20,9 +20,8 @@ import java.util.concurrent.TimeUnit;
 public class RedisLockDemo2 implements Runnable {
     private Integer sum = 0;
     private static final String rAtomicName = "demo_atomic";
+
     private static Redisson redisson = null;
-
-
     private static Config config = new Config();
 
     static {
@@ -54,25 +53,13 @@ public class RedisLockDemo2 implements Runnable {
         }
     }
 
-    /**
-     * 获取redis中的原子ID
-     */
-    public static Long nextID() {
-        RAtomicLong atomicLong = redisson.getAtomicLong(rAtomicName);
-        atomicLong.incrementAndGet();
-        return atomicLong.get();
-    }
-
     public static void main(String[] args) throws InterruptedException {
-        System.out.println(redisson);
-        System.out.println(null != redisson);
-
         RedisLockDemo2 rd = new RedisLockDemo2();
         long start = System.currentTimeMillis();
 
-        ExecutorService service = Executors.newFixedThreadPool(5);
+        ExecutorService service = Executors.newFixedThreadPool(30);
         List<Future<?>> futureTasks = new ArrayList<>();
-        for (int i = 0; i < 5; i++) {
+        for (int i = 1; i <= 10; i++) {
             futureTasks.add(service.submit(rd));
         }
         try {
@@ -96,14 +83,17 @@ public class RedisLockDemo2 implements Runnable {
                 // lock提供带timeout参数，timeout结束强制解锁，防止死锁 (不是等待锁时间而是最大执行时间, 方法会一直阻塞直到获得锁)
                 // 不设置则默认设置30s;
 
-                rLock.lock(3000, TimeUnit.MILLISECONDS);
+                rLock.lock(5, TimeUnit.SECONDS);
                 ifLock = true;
             }
 
             if (true) {
                 // 即使该锁是公平锁fairLock，使用tryLock()的方式获取锁也会是非公平的方式，只要获取锁时该锁可用那么就会直接获取并返回true。
                 // 这种直接插入的特性在一些特定场景是很有用的。但是如果就是想使用公平的方式的话，可以试一试tryLock(0, TimeUnit.SECONDS)，几乎跟公平锁没区别，只是会监测中断事件
-                ifLock = rLock.tryLock(6000, 3000, TimeUnit.MILLISECONDS);
+                /*
+                 * 在waitTime内 重复尝试获取锁 直到超过等待时间或成功获取锁(等待获得锁总时长:waitTime;每次获取锁时间:leaseTime)
+                 */
+                ifLock = rLock.tryLock(5, 3, TimeUnit.SECONDS);
                 // ifLock = rLock.tryLock();
             }
         } catch (Exception e) {
@@ -125,73 +115,45 @@ public class RedisLockDemo2 implements Runnable {
         String lockStr = "lockStr007";
         System.out.println(lockStr);
 
-        // option one
-        if (true) {
-            for (int i = 0; i < 5; i++) {
-                try {
-                    while (true) {
-                        boolean ifLock = RedisLockDemo2.getLock(lockStr);
-                        if (ifLock) {
-                            System.out.println(Thread.currentThread().getName() + " A " + i);
-                            Integer nt = sum;
-                            Thread.sleep(300);
-                            sum = nt + 1;
-                            System.out.println(Thread.currentThread().getName() + " B " + i);
-                            Thread.sleep(300);
-                            System.out.println(Thread.currentThread().getName() + " C " + i + "-------------------> " + sum);
-                            for (int j = 1; j <= 4; j++) {
-                                Thread.sleep(500);
-                                System.out.print("*" + j + "\t");
-                            }
-                            System.out.println();
-
-                            // 当线程等待超时时间设置为3000ms,则上方执行完毕以后在这里等待的10ms处发生超时,此时第二个线程马上获得锁进入
-                            // 所以第二个线程的"thread-A 0"会在第一个线程的"thread-release"日志前打印出来(实际情况可能有微小误差,因为线程执行开始时间不定)
-                            Thread.sleep(300);
-
-                            break;
-                        }
-
-                        System.out.println(Thread.currentThread().getName() + " D " + i);
-                    }
-                } catch (Exception e) {
-                    e.printStackTrace();
-                } finally {
-                    try {
-                        System.out.println(Thread.currentThread().getName() + " release " + "\n");
-                        releaseLock(redisson, lockStr);
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
+        try {
+            boolean ifLock = RedisLockDemo2.getLock(lockStr);
+            // total: 1000ms=1s
+            if (ifLock) {
+                System.out.println(Thread.currentThread().getName() + " A ");
+                Integer nt = sum;
+                Thread.sleep(200);
+                sum = nt + 1;
+                System.out.println(Thread.currentThread().getName() + " B ");
+                Thread.sleep(200);
+                for (int j = 1; j <= 4; j++) {
+                    Thread.sleep(100);
+                    System.out.print("*" + j + "\t");
                 }
+                System.out.println();
+                System.out.println(Thread.currentThread().getName() + " C " + "-------------------> " + sum);
+                Thread.sleep(200);
+                System.out.println(Thread.currentThread().getName() + " D ");
+            } else {
+                System.out.println(Thread.currentThread().getName() + " - E");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                System.out.println(Thread.currentThread().getName() + " release " + "\n");
+                releaseLock(redisson, lockStr);
+            } catch (Exception e) {
+                e.printStackTrace();
             }
         }
+    }
 
-        // option two
-        if (false) {
-            System.out.println("3---" + Thread.currentThread().getName() + " begin");
-            for (int j = 0; j < 100; j++) {
-                try {
-                    while (true) {
-                        boolean ifLock = RedisLockDemo2.getLock(lockStr);
-                        if (ifLock) {
-                            System.out.println(Thread.currentThread().getName() + " - " + sum);
-                            sum++;
-
-                            break;
-                        }
-                    }
-                } catch (Exception e) {
-                    e.printStackTrace();
-                } finally {
-                    try {
-                        releaseLock(redisson, lockStr);
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-            System.out.println("3---" + Thread.currentThread().getName() + " done " + sum);
-        }
+    /**
+     * 获取redis中的原子ID
+     */
+    public static Long nextID() {
+        RAtomicLong atomicLong = redisson.getAtomicLong(rAtomicName);
+        atomicLong.incrementAndGet();
+        return atomicLong.get();
     }
 }
