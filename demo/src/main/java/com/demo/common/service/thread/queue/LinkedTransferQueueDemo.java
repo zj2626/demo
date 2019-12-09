fc76682d7a8821b1fad118740c3f096281950d87
jin.zhang <jin.zhang@yitaichang.cn>
2019-12-09 18:35:03 +0800

    待完成

--- demo/src/main/java/com/demo/common/service/thread/queue/LinkedTransferQueueDemo.java
+++ demo/src/main/java/com/demo/common/service/thread/queue/LinkedTransferQueueDemo.java
@@ -8,6 +8,7 @@ import org.junit.Test;
 import java.util.Map;
 import java.util.Random;
 import java.util.concurrent.LinkedTransferQueue;
+import java.util.concurrent.TimeUnit;
 
 public class LinkedTransferQueueDemo  extends MyExcutor {
     LinkedTransferQueue<Integer> queue = new LinkedTransferQueue<>();
@@ -17,7 +18,7 @@ public class LinkedTransferQueueDemo  extends MyExcutor {
         ThreadDemo producerThread = new ThreadDemo(this);
         producerThread.execute(Params.builder().size(1).build());
         ThreadDemo consumerThread = new ThreadDemo(this);
-        consumerThread.execute(Params.builder().size(1).type("2").build());
+//        consumerThread.execute(Params.builder().size(1).type("2").build());
         producerThread.futureGet();
         consumerThread.futureGet();
     }
@@ -26,8 +27,10 @@ public class LinkedTransferQueueDemo  extends MyExcutor {
     public String doExcute(Map<String, String> parameter) throws Exception {
         while (true) {
             try {
-                System.out.println(Thread.currentThread().getName() + " reentrantLock produce ---> " + queue.size());
-                boolean success = queue.offer(new Random().nextInt(50));
+                boolean success = true;
+                success = queue.offer(new Random().nextInt(50), 5, TimeUnit.SECONDS);
+                queue.put(new Random().nextInt(50));
+                System.out.println(Thread.currentThread().getName() + " produce ---> " + queue.size() + " ==== " + success);
             } catch (Exception e) {
                 e.printStackTrace();
             }
@@ -40,8 +43,10 @@ public class LinkedTransferQueueDemo  extends MyExcutor {
     public String doExcuteRead(Map<String, String> ignore) throws Exception {
         while (true) {
             try {
-                System.out.println(Thread.currentThread().getName() + " reentrantLock consume -> " + queue.size());
-                Object obj = queue.poll();
+                Object obj = null;
+//                obj = queue.poll();
+                obj = queue.take();
+                System.out.println(Thread.currentThread().getName() + " consume -> " + queue.size() + " ==== " + obj);
             } catch (Exception e) {
                 e.printStackTrace();
             }
--- demo/src/main/java/com/demo/common/service/thread/queue/SynchronousQueueDemo.java
+++ demo/src/main/java/com/demo/common/service/thread/queue/SynchronousQueueDemo.java
@@ -8,16 +8,23 @@ import org.junit.Test;
 import java.util.Map;
 import java.util.Random;
 import java.util.concurrent.SynchronousQueue;
+import java.util.concurrent.TimeUnit;
 
 public class SynchronousQueueDemo extends MyExcutor {
     SynchronousQueue<Integer> queue = new SynchronousQueue<>();
 
+    /**
+     * SynchronousQueue没有容量，是无缓冲等待队列，是一个不存储元素的阻塞队列，会直接将任务交给消费者，
+     * 必须等队列中的添加元素被消费后才能继续添加新的元素。
+     *
+     * @throws InterruptedException
+     */
     @Test
     public void test() throws InterruptedException {
         ThreadDemo producerThread = new ThreadDemo(this);
         producerThread.execute(Params.builder().size(1).build());
         ThreadDemo consumerThread = new ThreadDemo(this);
-        consumerThread.execute(Params.builder().size(1).type("2").build());
+//        consumerThread.execute(Params.builder().size(1).type("2").build());
         producerThread.futureGet();
         consumerThread.futureGet();
     }
@@ -26,8 +33,10 @@ public class SynchronousQueueDemo extends MyExcutor {
     public String doExcute(Map<String, String> parameter) throws Exception {
         while (true) {
             try {
-                System.out.println(Thread.currentThread().getName() + " reentrantLock produce ---> " + queue.size());
-                boolean success = queue.offer(new Random().nextInt(50));
+                boolean success = true;
+//                success = queue.offer(new Random().nextInt(50), 5, TimeUnit.SECONDS);
+                queue.put(new Random().nextInt(50));
+                System.out.println(Thread.currentThread().getName() + " produce ---> " + queue.size() + " ==== " + success);
             } catch (Exception e) {
                 e.printStackTrace();
             }
@@ -40,8 +49,10 @@ public class SynchronousQueueDemo extends MyExcutor {
     public String doExcuteRead(Map<String, String> ignore) throws Exception {
         while (true) {
             try {
-                System.out.println(Thread.currentThread().getName() + " reentrantLock consume -> " + queue.size());
-                Object obj = queue.poll();
+                Object obj = null;
+                obj = queue.poll();
+//                obj = queue.take();
+                System.out.println(Thread.currentThread().getName() + " consume -> " + queue.size() + " ==== " + obj);
             } catch (Exception e) {
                 e.printStackTrace();
             }
