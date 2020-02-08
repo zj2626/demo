3f4d6200f1b2b1737b2fbfd4e5d45bb5d698c08a
zj2626 <zhangj2626>
2020-02-08 19:44:24 +0800

    test

--- spring-m-boot-parent/spring-m-boot/src/main/java/hello/config/KafkaConsumerBean.java
+++ spring-m-boot-parent/spring-m-boot/src/main/java/hello/config/KafkaConsumerBean.java
@@ -9,13 +9,22 @@ import org.springframework.stereotype.Component;
 @ConfigurationProperties(prefix = "spring.kafka.consumer")
 //@PropertySource(value = "application.properties")
 public class KafkaConsumerBean {
+
     private String bootstrapServers;
+
     private String groupId;
+
     private Boolean enableAutoCommit;
+
     private Integer autoCommitInterval;
+
     private String autoOffsetReset;
+
     private String keyDeserializer;
+
     private String valueDeserializer;
+
     private Integer maxPollRecords;
+
     private Integer sessionTimeout;
 }
