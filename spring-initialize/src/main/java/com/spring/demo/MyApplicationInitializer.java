1102c6ec38e3df8d5801c572e47ec9cb67023328
zj2626 <ay180629@outlook.com>
2020-07-01 11:29:19 +0800

    fix

--- spring-initialize/src/main/java/com/spring/demo/MyApplicationInitializer.java
+++ spring-initialize/src/main/java/com/spring/demo/MyApplicationInitializer.java
@@ -15,7 +15,7 @@ import javax.servlet.ServletRegistration;
  * 在容器启动的时候会调用到SpringServletContainerInitializer#onStartup方法
  * 其会通过注解@HandlesTypes({WebApplicationInitializer.class})把所有的WebApplicationInitializer实现都赋值到onStartup的参数Set<>webAppInitializerClasses中
  */
-public class MyApplicationInitializeDemo implements WebApplicationInitializer {
+public class MyApplicationInitializer implements WebApplicationInitializer {
     @Override
     public void onStartup(ServletContext servletContext) throws ServletException {
         // 初始化非web的spring环境
