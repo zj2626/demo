5d977bbbe4e3a7da891f77bb7b8e2142d1bf5d43
zj2626 <ay180629@outlook.com>
2020-11-04 11:33:00 +0800

    fix

---
+++ demo/src/main/java/com/demo/common/service/basic/ExtendsTest.java
@@ -0,0 +1,31 @@
+package com.demo.common.service.basic;
+
+/**
+ * @author zj2626
+ * @name ExtendsTest
+ * @description
+ * @create 2020-11-04 11:25
+ **/
+public class ExtendsTest {
+
+    public static void main(String[] args) {
+//        C c = new C();
+//        System.out.println(c);
+
+        System.out.println(C.abc);
+    }
+}
+
+class P {
+    public static int abc = 123;
+
+    static {
+        System.out.println("PPPP");
+    }
+}
+
+class C extends P {
+    static {
+        System.out.println("CCCC");
+    }
+}
