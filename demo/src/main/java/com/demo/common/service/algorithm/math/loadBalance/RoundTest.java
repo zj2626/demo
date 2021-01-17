0c1db1a73e2cc8aef7794e5ed6b6a4982c6ee294
zj2626 <zhangj2626>
2021-01-17 15:22:20 +0800

    math

--- demo/src/main/java/com/demo/common/service/algorithm/math/loadBalance/RoundTest.java
+++ demo/src/main/java/com/demo/common/service/algorithm/math/loadBalance/RoundTest.java
@@ -1,36 +1,55 @@
 package com.demo.common.service.algorithm.math.loadBalance;
 
+import com.demo.common.service.algorithm.math.loadBalance.bo.WeightBo;
 import org.junit.Test;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /*
 负载均衡算法: 轮询
  */
 public class RoundTest {
 
+    private int current = 0;
+    private static List<Server> orgin = Server.list();
+    private static List<WeightBo> list;
+
+    static {
+        list = orgin.stream().map(o -> {
+            WeightBo weightBo = new WeightBo();
+            weightBo.setIp(o.getIp());
+            weightBo.setScore(o.getScore());
+            weightBo.setOriginWeight(o.getScore());
+            weightBo.setCurrentWeight(o.getScore());
+            return weightBo;
+        }).collect(Collectors.toList());
+    }
+
     // 简单轮询
     @Test
     public void test() {
-        List<Server> list = Server.list();
-
-        int total = list.stream().map(Server::getScore).reduce(Integer::sum).get();
-
-        int k = 0;
         for (int i = 0; i < 10; i++) {
-            int index = k;
-            for (Server server : list) {
-                if (index < server.getScore()) {
-                    System.out.println(i + " ---> " + server);
-                    break;
-                }
-                index -= server.getScore();
-            }
+            getServer();
+        }
+    }
 
-            k++;
-            if(k == total){
-                k = 0;
+    private void getServer() {
+        int total = orgin.stream().map(Server::getScore).reduce(Integer::sum).get();
+
+        int index = current;
+        for (Server server : orgin) {
+            if (index < server.getScore()) {
+                System.out.println(" ---> " + server);
+                break;
             }
+            index -= server.getScore();
+        }
+
+        current++;
+        if (current == total) {
+            current = 0;
         }
     }
 
@@ -39,28 +58,43 @@ public class RoundTest {
      * "192.168.1.1", 6
      * "192.168.1.2", 1
      * "192.168.1.3", 3
-     *
+     * <p>
      * 循环开始: >  当前权重: [6,1,3]   权重和 = [10]
      * > *    当前权重         最大权重   最大权重ip         最大权重-权重和
-     * 0.*    6,1,3           6         192.168.1.1      -4,1,3
-     * 1.*    2,2,6           6         192.168.1.3      2,2,-4
-     * 2.*    8,3,-1          8         192.168.1.1      -2,3,1
-     * 3.*    4,4,4           4         192.168.1.1      -6,4,4
-     * 4.*    0,5,7           7         192.168.1.3      0,5,-3
-     * 5.*    6,6,0           6         192.168.1.1      -4,6,0
-     * 6.*    2,7,3           7         192.168.1.2      2,-3,3
-     * 7.*    8,-2,6          8         192.168.1.1      -2,-2,6
-     * 8.*    4,-1,9          9         192.168.1.3      4,-1,-1
-     * 9.*    10,0,2         10         192.168.1.1      0,0,2
-     * < *    6,1,3 ...
-     *
+     * 0.*    [6, 1, 3]       6         192.168.1.1      [-4, 1, 3]
+     * 1.*    [2, 2, 6]       6         192.168.1.3      [2, 2, -4]
+     * 2.*    [8, 3, -1]      8         192.168.1.1      [-2, 3, -1]
+     * 3.*    [4, 4, 2]       4         192.168.1.1      [-6, 4, 2]
+     * 4.*    [0, 5, 5]       5         192.168.1.2      [0, -5, 5]
+     * 5.*    [6, -4, 8]      8         192.168.1.3      [6, -4, -2]
+     * 6.*    [12, -3, 1]     12        192.168.1.1      [2, -3, 1]
+     * 7.*    [8, -2, 4]      8         192.168.1.1      [-2, -2, 4]
+     * 8.*    [4, -1, 7]      7         192.168.1.3      [4, -1, -3]
+     * 9.*    [10, 0, 0]     10         192.168.1.1      [0, 0, 0]
+     * < *    [6, 1, 3]  ... ...
      */
     @Test
     public void testWithScore() {
-        List<Server> list = Server.list();
+        for (int i = 0; i < 15; i++) {
+            getServerWithScore();
+        }
+    }
 
+    private void getServerWithScore() {
         int total = list.stream().map(Server::getScore).reduce(Integer::sum).get();
 
+        WeightBo server = null;
+        for (WeightBo bo : list) {
+            if(null == server || server.getCurrentWeight() < bo.getCurrentWeight()){
+                server = bo;
+            }
+        }
+        System.out.println(" ---> " + server.getIp());
 
+        // 设置下次权重
+        server.setCurrentWeight(server.getCurrentWeight() - total);
+        for (WeightBo bo : list) {
+            bo.setCurrentWeight(bo.getCurrentWeight() + bo.getOriginWeight());
+        }
     }
 }
