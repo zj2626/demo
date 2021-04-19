2ff0733f7da4f036acc8c72e59c7aba2c211748b
zj2626 <ay180629@outlook.com>
2021-04-19 14:20:10 +0800

    HashMap模拟 resize

--- demo/src/main/java/com/demo/common/service/collection/map/SourceMapDemo.java
+++ demo/src/main/java/com/demo/common/service/collection/map/SourceMapDemo.java
@@ -7,16 +7,25 @@ import org.junit.Test;
 public class SourceMapDemo {
     @Test
     public void addTest() {
-        HashMapDemo<String, String> map = new HashMapDemo<>(5);
+        HashMapDemo<Object, Object> map = new HashMapDemo<>(5);
         System.out.println(map);
 
-        map.put("4b4_0", null);
-        map.put("25k_1", null);
-        map.put("H47_2", null);
-        map.put("yl6_3", null);
-        map.put("gu5_4", null);
-        map.put("4cm_5", null);
-        map.put("V78_6", null);
+        map.put(8, null);
+        map.put(19, null);
+        map.put(29, null);
+        map.put(39, null);
+        map.put(49, null);
+        map.put(59, null);
+        map.put(69, null);
+
+//        map.put("4b4_0", null);
+//        map.put("25k_1", null);
+//        map.put("H47_2", null);
+//        map.put("yl6_3", null);
+//        map.put("gu5_4", null);
+//        map.put("4cm_5", null);
+//        map.put("V78_6", null);
+
 //        for (int i = 7; i < 8; i++) {
 //            map.put(RandomStringUtils.getRandomString(3) + "_" + i, null);
 //        }
@@ -27,5 +36,13 @@ public class SourceMapDemo {
 
         String key = "a";
         System.out.println(map.get(key));
+    }
+
+    @Test
+    public void demo() {
+        System.out.println(19 & 7);
+        System.out.println(19 & 8);
+        System.out.println(59 & 7);
+        System.out.println(59 & 8);
     }
 }
