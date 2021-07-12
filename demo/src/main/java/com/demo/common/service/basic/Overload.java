aa6481a7c8f82e92cbb93b9d7d69e412e336de1c
zj2626 <ay180629@outlook.com>
2021-07-12 18:29:34 +0800

    重载的类型转换

--- demo/src/main/java/com/demo/common/service/basic/Overload.java
+++ demo/src/main/java/com/demo/common/service/basic/Overload.java
@@ -44,12 +44,16 @@ public class Overload {
         System.out.println("hello char……");
     }
 
-    public static void sayHello(Serializable arg) {
-        System.out.println("hello Serializable");
+    public static void sayHello(int... arg) {
+        System.out.println("hello int……");
     }
 
+//    public static void sayHello(Serializable arg) {
+//        System.out.println("hello Serializable");
+//    }
+
     public static void main(String[] args) {
-        // 类型自动转型 : 按照char-＞int-＞long-＞float-＞double->Character->Serializable->Object->Array 的顺序转型进行匹配。但不会匹配到byte和short类型的重载，因为char到byte或short的转型是不安全的
+        // 类型自动转型(优先级) : 按照char-＞int-＞long-＞float-＞double->Character->Serializable->Object->变长参数(Array) 的顺序转型进行匹配。但不会匹配到byte和short类型的重载，因为char到byte或short的转型是不安全的
         sayHello('a');
         // 1. 调用
         // 2. 注释掉sayHello(char arg)方法  调用
